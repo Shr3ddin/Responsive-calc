42c46decbf7d452c1bb1c087bc0b9cd838f5a92c
Shr3ddin <patrykprzybylek94@gmail.com>
2023-12-25 21:45:56 +0100

    Styling, history design added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const prevNum = document.querySelector('.prevNum');
+const mathSign = document.querySelector('.mathSign');
+const currentNum = document.querySelector('.currentNum');
+const numBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+
+let result = '';
+let maxChars = 11;
+
+function handleNums() {
+	if (currentNum.textContent.length > maxChars) return;
+
+	if (this.textContent === '.' && currentNum.textContent.includes('.')) return;
+
+	currentNum.textContent += this.textContent;
+}
+
+numBtns.forEach(btn => btn.addEventListener('click', handleNums));
