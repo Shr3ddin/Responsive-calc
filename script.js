d2df2ccee8cd58e55297d94081d5f7e17b056ae1
Shr3ddin <patrykprzybylek94@gmail.com>
2023-12-26 22:05:24 +0100

    calc logic implemented

--- script.js
+++ script.js
@@ -7,14 +7,69 @@ const equals = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
 
 let result = '';
-let maxChars = 11;
 
 function handleNums() {
-	if (currentNum.textContent.length > maxChars) return;
-
 	if (this.textContent === '.' && currentNum.textContent.includes('.')) return;
+
+	if (this.textContent === '.' && currentNum.textContent === '') return (currentNum.textContent = '0.');
 
 	currentNum.textContent += this.textContent;
 }
 
+function handleOperators() {
+	if (currentNum.textContent === '' && this.textContent === '-') {
+		currentNum.textContent = '-';
+		return;
+	} else if (currentNum.textContent === '') {
+		return;
+	}
+
+	if (mathSign.textContent !== '') {
+		calculate();
+	}
+
+	prevNum.textContent = currentNum.textContent;
+	mathSign.textContent = this.textContent;
+	currentNum.textContent = '';
+}
+
+function calculate() {
+	if (prevNum.textContent === '' || currentNum.textContent === '') return;
+
+	let a = parseFloat(currentNum.textContent);
+	let b = parseFloat(prevNum.textContent);
+	let operator = mathSign.textContent;
+
+	switch (operator) {
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = b - a;
+			break;
+		case 'x':
+			result = a * b;
+			break;
+		case '/':
+			result = b / a;
+			break;
+		case '^':
+			result = b ** a;
+			break;
+	}
+
+	currentNum.textContent = result;
+	prevNum.textContent = '';
+	mathSign.textContent = '';
+}
+
+function clearOperation() {
+	prevNum.textContent = '';
+	mathSign.textContent = '';
+	currentNum.textContent = '';
+}
+
 numBtns.forEach(btn => btn.addEventListener('click', handleNums));
+operatorBtns.forEach(btn => btn.addEventListener('click', handleOperators));
+equals.addEventListener('click', calculate);
+clear.addEventListener('click', clearOperation);
